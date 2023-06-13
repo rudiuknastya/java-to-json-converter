7c02bf7d2e668b777434b41f8851bad61dc0e244
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2023-06-13 14:36:36 +0300

    Created classes Person, Converter and Main

---
+++ Main.java
@@ -0,0 +1,7 @@
+package task6;
+
+import java.lang.reflect.*;
+
+public class Main {
+
+}
---
+++ Person.java
@@ -0,0 +1,30 @@
+package task6;
+
+import java.util.*;
+public class Person {
+    private String name = "Alex";
+    private Integer age = 23;
+    private boolean married = true;
+    private List<String> hobbies = new ArrayList<>();
+    private Map<String, String>languages = new HashMap<>();
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+
+    public void setHobbies(String hobbie) {
+        hobbies.add(hobbie);
+    }
+
+    public void setLanguages(String key, String value) {
+        languages.put(key,value);
+    }
+}
